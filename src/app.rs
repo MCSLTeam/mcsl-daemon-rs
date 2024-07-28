173ed410f3bd687cd75826f1adcc2d6283128971
AresConnor <39639716+AresConnor@users.noreply.github.com>
2024-07-28 10:54:15 +0800

    feat: graceful shutdown

--- src/app.rs
+++ src/app.rs
@@ -8,7 +8,8 @@ use jsonwebtoken::{encode, EncodingKey, Header};
 use log::info;
 use serde::Deserialize;
 use tokio::net::TcpListener;
-
+use fastwebsockets::{OpCode, upgrade, WebSocketError};
+use tokio::signal;
 use crate::storage::AppConfig;
 use crate::user;
 use crate::user::{Users, UsersManager};
@@ -56,6 +57,65 @@ async fn login_handler(
     };
 }
 
+async fn handle_client(fut: upgrade::UpgradeFut) -> Result<(), WebSocketError> {
+    let mut ws = fastwebsockets::FragmentCollector::new(fut.await?);
+
+    loop {
+        let frame = ws.read_frame().await?;
+        match frame.opcode {
+            OpCode::Close => break,
+            OpCode::Text | OpCode::Binary => {
+                ws.write_frame(frame).await?;
+            }
+            _ => {}
+        }
+    }
+
+    Ok(())
+}
+async fn ws_handler(
+    State(_app_state): State<Arc<AppState>>,
+    ws: upgrade::IncomingUpgrade,
+) -> impl IntoResponse {
+    let (resp, fut) = ws.upgrade().unwrap();
+    tokio::task::spawn(async move {
+        if let Err(e) = handle_client(fut).await {
+            eprintln!("Error in websocket connection: {}", e);
+        }
+    });
+    resp
+}
+
+// 返回一个 future，当接收到 Ctrl+C 或者其他关闭信号时完成
+async fn shutdown_signal() {
+
+    // 用于接收 Ctrl+C 信号
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("Failed to install Ctrl+C handler");
+    };
+
+    // 用于接收 SIGTERM 信号
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("Failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    println!("App shutting down...");
+}
+
 struct AppState {
     users: Arc<Users>,
     config: AppConfig,
@@ -76,9 +136,12 @@ impl App {
         }
     }
 
+
+
+
     pub async fn start(&self) -> anyhow::Result<()> {
         let listener = TcpListener::bind(format!("127.0.0.1:{}", &self.config.port)).await?;
-        info!("Server started at http://127.0.0.1:{}", &self.config.port);
+        info!("App started at http://127.0.0.1:{}", &self.config.port);
 
         let app_state = Arc::new(AppState {
             users: self.users.clone(),
@@ -87,8 +150,12 @@ impl App {
 
         let app = Router::new()
             .route("/login", get(login_handler))
+            .route("/api/v1", get(ws_handler))
             .with_state(app_state);
-        axum::serve(listener, app).await?;
+
+        axum::serve(listener, app)
+            .with_graceful_shutdown(shutdown_signal())
+            .await?;
         Ok(())
     }
 }
