a2f8d01b63c1e24faafbf03dd5cf7b070e8c8c73
Connor Zheng <2837434884@qq.com>
2024-10-12 16:01:07 +0800

    refactor: default app config

--- src/storage/app_config.rs
+++ src/storage/app_config.rs
@@ -3,8 +3,10 @@ use serde::{Deserialize, Serialize};
 use super::file::{Config, FileIoWithBackup};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
+/// immutable through full lifetime of app, unless restart app.
 pub struct AppConfig {
     pub port: u16,
+    pub file_download_sessions: u8,
 }
 
 impl FileIoWithBackup for AppConfig {}
@@ -15,6 +17,15 @@ impl Config for AppConfig {
 
 impl AppConfig {
     pub fn new() -> AppConfig {
-        Self::load_config_or_default("config.json", || AppConfig { port: 11452 }).unwrap()
+        Self::load_config_or_default("config.json", Self::default).unwrap()
+    }
+}
+
+impl Default for AppConfig {
+    fn default() -> Self {
+        AppConfig {
+            port: 11452,
+            file_download_sessions: 3,
+        }
     }
 }
